28267cb8b6005252a5a93637946af3f5b0ef6f18
Nikita Sytnik <quazzz13@gmail.com>
2024-04-08 19:12:07 +0200

    modals added

---
+++ js/modals.js
@@ -0,0 +1,31 @@
+function toggleModal(id, event){
+    event.stopPropagation();
+    let sub = document.getElementById(id);
+
+    if(sub.style.display === "flex"){
+        sub.style.display = "none";
+        let hid = document.querySelector("body");
+        hid.style.overflow = "auto";
+    }else{
+        sub.style.display = "flex";
+        let hid = document.querySelector("body");
+        hid.style.overflow = "hidden";
+    }
+}
+
+document.getElementById("subscribe").addEventListener("click", (event) => toggleModal("modal__subscribe", event))
+
+document.getElementById("send__cv").addEventListener("click", (event) => toggleModal("modal__cv", event))
+
+document.getElementById("send__button-sub").addEventListener("click", (event) => toggleModal("modal__subscribe", event))
+
+document.getElementById("send__button-cv").addEventListener("click", (event) => toggleModal("modal__cv", event))
+
+document.getElementById("modal__cross-1").addEventListener("click", (event) => toggleModal("modal__subscribe", event))
+
+document.getElementById("modal__cross-2").addEventListener("click", (event) => toggleModal("modal__cv", event))
+
+document.getElementById("modal__subscribe").addEventListener("click", (event) => toggleModal("modal__subscribe", event))
+
+document.getElementById("modal__cv").addEventListener("click", (event) => toggleModal("modal__cv", event))
+
